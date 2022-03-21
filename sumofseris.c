4b9fec8106dfbbe37044db8a08e1fc0b0f2b3ba1
AayushiTyagi41 <101922146+AayushiTyagi41@users.noreply.github.com>
2022-03-21 20:51:56 +0530

    Update sumofseris.c

--- sumofseris.c
+++ sumofseris.c
@@ -2,7 +2,7 @@
 int main()
 {
    int i,n,sum=0;
-   printf("siddhant\n");
+   printf("aayushi\n");
    printf("Input Value of terms : ");
    scanf("%d",&n);
    printf("\nThe first %d natural numbers are:\n",n);
