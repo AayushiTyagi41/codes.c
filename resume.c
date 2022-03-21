76400a652c8fa37b9c5f688b8f1d5d80383b2634
AayushiTyagi41 <101922146+AayushiTyagi41@users.noreply.github.com>
2022-03-21 20:37:31 +0530

    Update resume.c

--- resume.c
+++ resume.c
@@ -2,8 +2,8 @@
 #include<conio.h>
 void main()
 {
-	printf("\n NAME:-Siddhant Gulati.");
-	printf("\n ADDRESS:-I-7/2,sec 16 rohini, delhi");
+	printf("\n NAME:-Aayushi Tyagi.");
+	printf("\n Vikaspuri, delhi");
 	printf("\n EDUCATION:- Pursuing Bachelor of Computer Applications");
 	printf("\n MOBILE:- +91 9911XXXXXX \n\n");
     getch();
