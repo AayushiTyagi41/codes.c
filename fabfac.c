51bae7854b3db9cbd8675aefdd2b432d4aa78eff
AayushiTyagi41 <101922146+AayushiTyagi41@users.noreply.github.com>
2022-03-21 20:04:31 +0530

    Update fabfac.c

--- fabfac.c
+++ fabfac.c
@@ -3,7 +3,7 @@ long int multiplyNumbers(int n);
 int fibonacci(int);
 int main() {
     int n,i;
-    printf("siddhant\n");
+    printf("aayushi\n");
     printf("Enter a positive integer:\n");
     scanf("%d",&n);
     printf("Factorial of %d = %ld\n", n, multiplyNumbers(n));
