55cf77d23436a1fa7119c4c2e90180f425f27276
AayushiTyagi41 <101922146+AayushiTyagi41@users.noreply.github.com>
2022-03-21 19:48:20 +0530

    Update array50.c

--- array50.c
+++ array50.c
@@ -3,7 +3,7 @@ int main() {
     int n, i;
     float num[100], sum = 0.0, avg;
 
-    printf("siddhant\n");
+    printf("aayushi\n");
     printf("Enter the numbers of elements: ");
     scanf("%d", &n);
 
