b34378b4c86cf431646396cb13e2c9f3921c36a6
AayushiTyagi41 <101922146+AayushiTyagi41@users.noreply.github.com>
2022-03-21 20:33:04 +0530

    Update pointerar.c

--- pointerar.c
+++ pointerar.c
@@ -5,7 +5,7 @@ int main( )
    int val[7] = { 11, 22, 33, 44, 55, 66, 77 } ;
    p = &val[0];
    q = &p;
-   printf("siddhant\n");
+   printf("aayushi\n");
    for ( int i = 0 ; i<7 ; i++ )
    {
       printf("val[%d]: value is %d \n", i, **q);
