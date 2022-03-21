fb54ac0f6fc3a95ac2f7ead9eaf0f640df7a56ac
AayushiTyagi41 <101922146+AayushiTyagi41@users.noreply.github.com>
2022-03-21 19:43:16 +0530

    Update aithmetic.c

--- aithmetic.c
+++ aithmetic.c
@@ -3,7 +3,7 @@
 int main()
 {
     int a=4,b=5;
-    printf("siddhant\n");
+    printf("aayushi\n");
     printf("sum of numbers:%d\n",a+b);
     printf("product of two numbers:%d\n",a*b);
     printf("division of two numbers:%d\n",a/b);
